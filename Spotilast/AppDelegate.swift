e443a4cade9e0569772237367e0a9e35432ed220
Jesse Ditson <jesse.ditson@gmail.com>
2015-11-12 14:28:02 -0800

    Initial Commit

---
+++ Spotilast/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Spotilast
+//
+//  Created by Jesse Ditson on 11/12/15.
+//  Copyright © 2015 Prix Fixe Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
